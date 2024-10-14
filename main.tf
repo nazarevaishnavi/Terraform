8c88451cca45f35d19b207f25a5e6ef26365d837
Vaishnavi Nazare <vaishnavi.nazare@idfy.com>
2024-10-14 22:40:45 +0530

    resolved configuration errors

--- main.tf
+++ main.tf
@@ -8,10 +8,10 @@ terraform {
   required_version = ">= 1.2.0"
 }
 provider "aws" {
-    region = "asia-south-1" 
+    region = "ap-south-1" 
 }
 resource "aws_instance" "app_server" {
-    ami    = "ami-201102"
+    ami    = "ami-0dee22c13ea7a9a67"
     instance_type = "t2.micro"
 
     tags = {
